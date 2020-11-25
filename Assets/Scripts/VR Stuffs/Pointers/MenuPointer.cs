cac05145add0eacff4e02d5e8759eabbaafa0cc9
Asclepiius.Unknown <asclepiius.unknown@gmail.com>
2020-11-25 15:35:08 +1100

    UI Pointer

---
+++ Assets/Scripts/VR Stuffs/Pointers/MenuPointer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MenuPointer : MonoBehaviour
+{
+    public float defaultLength = 5;
+    public GameObject dot;
+    public VRInputModule inputModule;
+
+    private LineRenderer lineRenderer;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    private void Update()
+    {
+        UpdateLine();
+    }
+
+    void UpdateLine()
+    {
+        //Use default or distance
+        PointerEventData data = inputModule.GetData();
+        float targetLength = (data.pointerCurrentRaycast.distance==0)?defaultLength:data.pointerCurrentRaycast.distance;
+
+        //Raycast
+        RaycastHit hit = CreateRaycast(targetLength);
+
+        //Default
+        Vector3 endPos = transform.position + (transform.forward * targetLength);
+
+        //Or based on hit
+        if (hit.collider != null)
+            endPos = hit.point;
+
+        //Set Position of the dot
+        dot.transform.position = endPos;
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, endPos);
+    }
+
+    private RaycastHit CreateRaycast(float _length)
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, transform.forward);
+        Physics.Raycast(ray, out hit, 5);
+
+        return hit;
+    }
+}
---
+++ Assets/Scripts/VR Stuffs/Pointers/VRInputModule.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.EventSystems;
+
+public class VRInputModule : BaseInputModule
+{
+    public Camera camera;
+    public SteamVR_Input_Sources targetSource;
+    public SteamVR_Action_Boolean clickAction;
+    public Canvas canvas;
+
+    private GameObject currentObject;
+    private PointerEventData data;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        data = new PointerEventData(eventSystem);
+
+        if (canvas != null)
+            if (VrHelper.VRIsPresent())
+                canvas.worldCamera = camera;
+    }
+
+    public override void Process()
+    {
+        data.Reset();
+        data.position = new Vector2(camera.pixelWidth / 2, camera.pixelHeight / 2);
+
+        eventSystem.RaycastAll(data, m_RaycastResultCache);
+        data.pointerCurrentRaycast = FindFirstRaycast(m_RaycastResultCache);
+        currentObject = data.pointerCurrentRaycast.gameObject;
+
+        m_RaycastResultCache.Clear();
+
+        HandlePointerExitAndEnter(data, currentObject);
+
+        if (clickAction.GetStateDown(targetSource))
+            ProcessRelease(data);
+    }
+
+    public PointerEventData GetData()
+    {
+        return data;
+    }
+
+    private void ProcessPress(PointerEventData _data)
+    {
+        data.pointerPressRaycast = data.pointerCurrentRaycast;
+
+        GameObject newPointerPress = ExecuteEvents.ExecuteHierarchy(currentObject, data, ExecuteEvents.pointerDownHandler);
+
+        if (newPointerPress == null)
+            newPointerPress = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentObject);
+
+        data.pressPosition = data.position;
+        data.pointerPress = newPointerPress;
+        data.rawPointerPress = currentObject;
+    }
+
+    private void ProcessRelease(PointerEventData _data)
+    {
+        ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerUpHandler);
+
+        GameObject pointerUpHandler = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentObject);
+
+        if(data.pointerPress == pointerUpHandler)
+        {
+            ExecuteEvents.Execute(data.pointerPress, data, ExecuteEvents.pointerClickHandler);
+        }
+
+        eventSystem.SetSelectedGameObject(null);
+
+        data.pressPosition = Vector2.zero;
+        data.pointerPress = null;
+        data.rawPointerPress = null;
+    }
+}
